6c2aa986182c8361e146d9099b8b535aaa5247f5
yaroslaws <gdok5@mail.ru>
2019-06-09 17:51:07 +0300

    Рабочая база р2 фильтр на сообщения

--- src/main/java/com/example/GreetingController.java
+++ src/main/java/com/example/GreetingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -36,9 +37,32 @@ public class GreetingController {
 
     @PostMapping
     public String add(@RequestParam String text, @RequestParam String tag,Map<String, Object> model){
-         Message message = new Message(text,tag);
 
-         messagesRepo.save(message);
+        Message message = new Message(text,tag);
+        messagesRepo.save(message);
+
+        Iterable<Message> messages = messagesRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+
+
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+
+        Iterable<Message> messages;
+
+        if(filter != null && !filter.isEmpty())
+        {
+            messages = messagesRepo.findByTag(filter);
+        }
+        else
+        {
+            messages = messagesRepo.findAll();
+        }
+
+        model.put("messages", messages);
 
         return "main";
     }
